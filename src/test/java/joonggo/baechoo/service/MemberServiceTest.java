29883daa98dcc4cfdef613200dafa1f008974625
N2\N2 <devkdh1121@gmail.com>
2025-05-09 15:45:52 +0900

    gitignore 수정 HomeController 생성

---
+++ src/test/java/joonggo/baechoo/service/MemberServiceTest.java
@@ -0,0 +1,28 @@
+package joonggo.baechoo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void listAll() {
+    }
+
+    @Test
+    void detailMember() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+
+    @Test
+    void updateMember() {
+    }
+}
